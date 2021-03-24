3307c2235f061d1af814d220f87f3affaa8b5d32
Marley Quinn-Parker <58849055+marleyqp@users.noreply.github.com>
2021-03-24 16:29:21 +0000

    Update installall.sh
    grammar go brrrr

--- installall.sh
+++ installall.sh
@@ -2,9 +2,8 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V1.1 (Stable) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
-echo "WARNING : IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
-echo "If you found bug pls create an issues in github ;)"
+echo -e "\e[31mWelcome to Uncursus Install Script V1.1 (Stable) By @Yaya4_4 and @marleyqp_.\e[0m"
+echo "WARNING : IM NOT RESPONSIBLE IF ANYTHING GOES WRONG"
 echo "Enjoy :)"
 echo "Starting..."
 echo "Installing Dependency's For The Installer"
@@ -41,7 +40,7 @@ dpkg -i --force-all /User/Documents/Uncursus/DebPatch/xz.deb
 dpkg -i --force-all /User/Documents/Uncursus/u0/*.deb
 echo "Done. Running Firmware Configuration (./firmware.sh)"
 /usr/libexec/firmware
-echo "BootStrap Installions Done. The Installer Clean The Installions"
+echo "Bootstrap Installations Done. The Installer Will Now Clean The Installations"
 rm -rf /User/Documents/Uncursus/
 rm /etc/apt/sources.list.d/odyssey.sources
 echo "All Done."
